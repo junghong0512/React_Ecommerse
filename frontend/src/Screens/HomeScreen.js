64e1e927b2d82000f7f4ec7f3da5277582251fab
Jung <junghong91@naver.com>
2020-06-15 18:02:54 +0900

    Fetching data from the local server

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+// import data from "../data";
+import axios from "axios";
 
 function HomeScreen(props) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <ul className="products">
-      {data.products.map((product, index) => (
-        <li key={index}>
+      {products.map((product) => (
+        <li key={product._id}>
           <div className="product">
             <Link to={`/product/${product._id}`}>
               <img
