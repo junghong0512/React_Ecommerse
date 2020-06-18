bff979ec2fd413acc3ab2f9df98e777b5d9af15b
Jung <junghong91@naver.com>
2020-06-18 15:07:00 +0900

    Product detail page gets data using Redux

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,59 +1,77 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+import { useSelector, useDispatch } from "react-redux"; // redux store value 조회
+import { detailsProduct } from "../actions/productActions";
 
 function ProductScreen(props) {
-  const product = data.products.find(
+  /* const product = data.products.find(
     (item) => item._id === props.match.params.id
-  );
+  ); */
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(detailsProduct(props.match.params.id));
+    return () => {
+      //
+    };
+  }, []);
 
   return (
     <div>
       <div className="back-to-home">
         <Link to="/">Back to Home</Link>
       </div>
-      <div className="details">
-        <div className="details-image">
-          <img src={product.image} alt="product" />
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <div className="details">
+          <div className="details-image">
+            <img src={product.image} alt="product" />
+          </div>
+          <div className="details-info">
+            <ul>
+              <li>
+                <h4>{product.name}</h4>
+              </li>
+              <li>
+                {product.rating} Stars ({product.numReviews} Reviews)
+              </li>
+              <li>
+                Price: <b>￦{product.price}</b>
+              </li>
+              <li>
+                Description:
+                <div>{product.description}</div>
+              </li>
+            </ul>
+          </div>
+          <div className="details-action">
+            <ul>
+              <li>Price: {product.price}</li>
+              <li>Status: {product.price}</li>
+              <li>
+                Qty:{" "}
+                <select>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                </select>
+              </li>
+              <li>
+                <button className="details-add-cart-btn primary">
+                  Add to Cart
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="details-info">
-          <ul>
-            <li>
-              <h4>{product.name}</h4>
-            </li>
-            <li>
-              {product.rating} Stars ({product.numReviews} Reviews)
-            </li>
-            <li>
-              Price: <b>￦{product.price}</b>
-            </li>
-            <li>
-              Description:
-              <div>{product.description}</div>
-            </li>
-          </ul>
-        </div>
-        <div className="details-action">
-          <ul>
-            <li>Price: {product.price}</li>
-            <li>Status: {product.price}</li>
-            <li>
-              Qty:{" "}
-              <select>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-              </select>
-            </li>
-            <li>
-              <button className="details-add-cart-btn primary">
-                Add to Cart
-              </button>
-            </li>
-          </ul>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,15 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { productListReducer } from "./reducer/productReducers";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducer/productReducers";
 
 const initialState = {};
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
