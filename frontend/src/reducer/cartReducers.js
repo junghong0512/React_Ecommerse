bc040a9bd47d1749af4af244c29a6e2924040eb4
Jung <junghong91@naver.com>
2020-06-20 17:00:59 +0900

    cartScreen is done with CSS, cart items are going to be stored in Cookie

--- frontend/src/reducer/cartReducers.js
+++ frontend/src/reducer/cartReducers.js
@@ -1,11 +1,11 @@
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 function cartReducer(state = { cartItems: [] }, action) {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
       const product = state.cartItems.find(
-        (itm) => itm.product === item.product
+        (itm) => itm.product === item.product // Check if the product is already in the cart
       );
       if (product) {
         return {
@@ -15,6 +15,12 @@ function cartReducer(state = { cartItems: [] }, action) {
         };
       }
       return { cartItems: [...state.cartItems, item] };
+    case CART_REMOVE_ITEM:
+      return {
+        cartItems: state.cartItems.filter(
+          (itm) => itm.product !== action.payload
+        ),
+      };
     default:
       return state;
   }
