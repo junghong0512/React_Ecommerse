d8a6df144565d4c54b5fd672603f21feb593bc58
Jung <junghong91@naver.com>
2020-06-30 20:25:56 +0900

    deploy heroku

--- backend/config.js
+++ backend/config.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 export default {
   MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/hongshop",
+  PORT: process.env.PORT || 5000,
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || "sb",
 };
