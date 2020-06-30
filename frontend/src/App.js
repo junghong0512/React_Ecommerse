f5fd9be85f4a0151ea5d44fec9b84b9fdc16a6c7
Jung <junghong91@naver.com>
2020-06-30 16:18:22 +0900

    Filtering and Sorting the Products, by getting the list of products using listProducts Action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -61,18 +61,18 @@ function App() {
           <button className="sidebar-close-button" onClick={closeMenu}>
             x
           </button>
-          <ul>
+          <ul className="categories">
             <li>
-              <a href="index.html">Pants</a>
+              <Link to="/category/Pants">Pants</Link>
             </li>
             <li>
-              <a href="index.html">Shirts</a>
+              <Link to="/category/Shirts">Shirts</Link>
             </li>
             <li>
-              <a href="index.html">Shoes</a>
+              <Link to="/category/Shoes">Shoes</Link>
             </li>
             <li>
-              <a href="index.html">Accesories</a>
+              <Link to="/category/Accesories">Accesories</Link>
             </li>
           </ul>
         </aside>
@@ -89,6 +89,7 @@ function App() {
             <Route path="/signin" component={SigninScreen} />
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/category/:id" component={HomeScreen} />
             <Route path="/" exact={true} component={HomeScreen} />
           </div>
         </main>
