59948c854062cd5f3415b422c5234ebaa60bbd90
Jung <junghong91@naver.com>
2020-07-01 13:13:57 +0900

    heroku

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,7 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 });
 
+//
 // app.use(express.static(path.join(__dirname, "/../frontend/build")));
 // app.get("*", (req, res) =>
 //   res.sendFile(path.join("${__dirname}/../frontend/build/index.html"))
