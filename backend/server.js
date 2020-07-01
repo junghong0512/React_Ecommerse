12d1fba7d3735b713126aa45da7bdfbcfdc02358
Jung <junghong91@naver.com>
2020-07-01 11:46:57 +0900

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,6 @@ app.get("/api/config/paypal", (req, res) => {
 //   res.sendFile(path.join("${__dirname}/../frontend/build/index.html"))
 // );
 
-app.listen(config.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server started at http://localhost:5000");
 });
