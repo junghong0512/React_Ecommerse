3cae88746cfaef4e5b12bcdc6e0d9539615a45b3
Jung <junghong91@naver.com>
2020-07-01 12:19:47 +0900

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import orderRoute from "./routes/orderRoute";
 
 const mongodbUrl = config.MONGODB_URL;
 
+const PORT = process.env.PORT || 5050;
+
 // Connect to the MongoDB
 mongoose
   .connect(mongodbUrl, {
@@ -32,6 +34,6 @@ app.get("/api/config/paypal", (req, res) => {
 //   res.sendFile(path.join("${__dirname}/../frontend/build/index.html"))
 // );
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server started at http://localhost:5000");
+app.listen(PORT, () => {
+  console.log("Server started at http://localhost:" + PORT);
 });
