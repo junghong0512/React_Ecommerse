2e3f719144a4124895117722cf3312333e8cf191
Jung <junghong91@naver.com>
2020-07-03 17:22:48 +0900

    Upload Product Image to the AWS S3

--- backend/config.js
+++ backend/config.js
@@ -7,4 +7,6 @@ export default {
   MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/hongshop",
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || "sb",
+  accessKeyId: process.env.accessKeyId || "accessKeyId",
+  secretAccessKey: process.env.secretAccessKey || "secretAccessKey",
 };
--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -1,5 +1,8 @@
 import express from "express";
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+import config from "../config";
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
@@ -18,6 +21,34 @@ const router = express.Router();
 router.post("/", upload.single("image"), (req, res) => {
   // Access to the file
   res.send(`/${req.file.path}`); // path is used for saving at the mongodb
+});
+
+// config aws is based on AccessID and SecretAccessKey, and Setting
+aws.config.update({
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey,
+});
+
+// access to s3 service
+const s3 = new aws.S3();
+
+// use multer-s3
+const storageS3 = multerS3({
+  s3,
+  bucket: "hongshop-bucket",
+  acl: "public-read", // after uploading file, the file is readable publicly
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  key(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+// create instance of multer
+const uploadS3 = multer({ storage: storageS3 });
+
+// create route
+router.post("/s3", uploadS3.single("image"), (req, res) => {
+  res.send(req.file.location); // location: url of the uploaded file
 });
 
 export default router;
